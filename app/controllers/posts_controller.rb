886aa6f9c933058d21d382070f95d3e00a435382
rjmmoore Moore <rob.moore@kellogg.northwestern.edu>
2025-03-02 23:40:37 +0000

    Attempt at the code, need to integrate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,28 @@ class PostsController < ApplicationController
     @post["body"] = params["body"]
     @post["image"] = params["image"]
     # TODO: assign logged-in user as user that created the post
+    def new
+      if @current_user
+        # Show the form
+      else
+        flash["notice"] = "You must be logged in to create a post."
+        redirect_to "/login"
+      end
+    end
     @post.save
     redirect_to "/posts"
+  end
+end
+
+def create
+  if @current_user
+    @post = Post.new
+    @post["content"] = params["content"]
+    @post["user_id"] = @current_user["id"]
+    @post.save
+    redirect_to "/posts"
+  else
+    flash["notice"] = "You must be logged in to create a post."
+    redirect_to "/login"
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,23 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
+      @user = User.find_by({ "email" => params["email"] })
+    
+      if @user && BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome back, #{@user["first_name"]}!"
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Invalid email or password."
+        redirect_to "/login"
+      end
     flash["notice"] = "Nope."
     redirect_to "/login"
   end
+end
+
+def destroy
+  session["user_id"] = nil
+  flash["notice"] = "Goodbye!"
+  redirect_to "/login"
 end
